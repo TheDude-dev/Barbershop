afe07a6867055c17d67bb6b5957641c19509db88
bonheurbaleks <bonheurbaleks@MacBook-Pro-bonheurbaleks.local>
2025-01-06 09:08:26 +0300

    refactor: none

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e52bf534438c0d1"; }
+/******/ 		__webpack_require__.h = function() { return "ac8cfd2d03d3163e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
